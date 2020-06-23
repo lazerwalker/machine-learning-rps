cf30bf1e1146ea06ffdae97df00ca59cce4c0b67
Em Lazer-Walker <me@lazerwalker.com>
2020-06-23 16:40:57 -0400

    Rip out most face game-specific logic

--- src/index.ts
+++ src/index.ts
@@ -8,18 +8,18 @@ function startVideoStream() {
   if (!navigator.mediaDevices.getUserMedia) return;
   navigator.mediaDevices
     .getUserMedia({ audio: false, video: { facingMode: "user" } })
-    .then(function(stream) {
+    .then(function (stream) {
       const video: HTMLVideoElement = document.querySelector("#webcam");
       video.srcObject = stream;
-      video.onloadedmetadata = e => {
+      video.onloadedmetadata = (e) => {
         video.play();
       };
 
-      video.onloadeddata = e => {
+      video.onloadeddata = (e) => {
         startTesting(video);
       };
     })
-    .catch(function(err) {
+    .catch(function (err) {
       alert(
         "An error has occurred loading your webcam feed. Try again, or maybe in a different browser?"
       );
@@ -39,64 +39,21 @@ function playAudio() {
 }
 
 let testingTimeout: number;
-let timerTimeout: number;
-let dateOfLastTouch = new Date();
-
-let highScoreInSeconds: number = 0;
-
-// TODO: I think there should be a toggle for showing system alerts?
-let alertIsVisible = false;
 
 function startTesting(video: HTMLVideoElement, interval: number = 100) {
   const title = document.getElementById("header");
-  const time = document.getElementById("time");
-
-  const highScore = document.getElementById("high-score");
-  const highScoreTime = document.getElementById("high-score-time");
 
   const loop = async () => {
     const isTouching = await checkFaceTouching(video);
 
-    const now = new Date();
-
     if (isTouching) {
-      if ((document.getElementById("play-sound") as HTMLInputElement).checked) {
-        playAudio();
-      }
+      playAudio();
       document.body.classList.add("touching");
-      title.innerText = "⚠️ YOU ARE TOUCHING YOUR FACE ⚠️";
-      dateOfLastTouch = now;
-
-      // alert() calls are "blocking" within the current thread
-      // This is a very sloppy semaphore lock to try to stop us from piling up alerts.
-      const showAlerts = (document.getElementById(
-        "show-alert"
-      ) as HTMLInputElement).checked;
-      if (showAlerts && !alertIsVisible) {
-        alertIsVisible = true;
-        alert("You touched your face!");
-        alertIsVisible = false;
-      }
     } else {
       document.body.classList.remove("touching");
-      title.innerText = "Don't Touch Your Face!";
     }
 
-    const differenceInSeconds = Math.floor(
-      (now.getTime() - dateOfLastTouch.getTime()) / 1000
-    );
-
-    if (differenceInSeconds >= highScoreInSeconds) {
-      highScoreInSeconds = differenceInSeconds;
-      highScore.classList.add("winning");
-    } else {
-      highScore.classList.remove("winning");
-    }
-
-    time.innerText = formatTime(differenceInSeconds);
     testingTimeout = (setTimeout(loop, interval) as unknown) as number;
-
-    highScoreTime.innerText = formatTime(highScoreInSeconds);
   };
 
   loop();
@@ -104,13 +61,6 @@ function startTesting(video: HTMLVideoElement, interval: number = 100) {
 
 function stopTesting() {
   clearTimeout(testingTimeout);
-  clearTimeout(timerTimeout);
-}
-
-function formatTime(seconds: number): string {
-  const minutes = Math.floor(seconds / 60);
-  const printSecs = ((seconds % 60).toString() as any).padStart(2, "0");
-  return `${minutes}:${printSecs}`;
 }
 
 async function checkFaceTouching(
@@ -119,14 +69,6 @@ async function checkFaceTouching(
   const result = await detect(video);
   if (!result) return;
   console.log(result);
-
-  const touchingDebugLabel = document.getElementById("chance-touching");
-  const notTouchingDebugLabel = document.getElementById("chance-not-touching");
-
-  touchingDebugLabel.innerText = `${(result.chanceTouching * 100).toFixed(1)}%`;
-  notTouchingDebugLabel.innerText = `${(result.chanceNotTouching * 100).toFixed(
-    1
-  )}%`;
 
   return result.chanceTouching > 0.01 && result.chanceNotTouching < 0.002;
 }
@@ -146,13 +88,4 @@ document.addEventListener("DOMContentLoaded", () => {
       safari.hidden = true;
     });
   }
-
-  document.querySelectorAll("input.sfx").forEach(el => {
-    el.addEventListener("change", e => {
-      const value = (e.target as HTMLInputElement).value;
-      console.log("Setting value", value);
-      currentAudio = value;
-      playAudio();
-    });
-  });
 });
