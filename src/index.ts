1891a5ac95c3346a46d4e4ccd192ef71c02655a9
David Smith <davidsmi@microsoft.com>
2020-07-08 16:37:28 -0700

    whitespace

--- src/index.ts
+++ src/index.ts
@@ -38,7 +38,7 @@ function stringForResult(type: ClassificationOption): string {
     [ClassificationOption.Lizard]: "lizard",
     [ClassificationOption.Spock]: "spock",
     [ClassificationOption.Nothing]: "nothing",
-  };  
+  };
   return map[type];
 }
 
